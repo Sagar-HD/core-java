80ce01805b7ea12a7dd26e58961c088a1738ac40
sagar9980274589 <sagarshaivaooo@gmail.com>
2026-02-26 22:37:01 +0530

    Theater :update

--- Theater/Customer.java
+++ Theater/Customer.java
@@ -1,7 +1,16 @@
 class Customer extends User{
+	@Override
+	public void showMenu(){
+		
+		System.out.println("will be implemented later");
+		
+		
+		
+	}
+	
 	public Customer(int userId,String name,String password){
 		super(userId,name,password);
-			System.out.println("user created");
+			System.out.println("customer created");
 	}
 	
 	
