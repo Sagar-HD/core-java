5f266c94d438fe29c392707297da7edbd400bab5
sagar9980274589 <sagarshaivaooo@gmail.com>
2026-03-30 09:19:44 +0530

    male class

---
+++ task 26/Male.java
@@ -0,0 +1,15 @@
+class Male implements Gender{
+	String name;
+	int age;
+	public Male(String name,int age){
+	
+		this.name=name;
+		this.age=age;
+	}
+	public void display(){
+		System.out.println("name :"+this.name+" age :"+this.age);
+	}
+	public String getName(){
+		return this.name;
+	}
+}
