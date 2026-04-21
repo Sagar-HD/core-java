bb971f9f3d8c52099ed632645e294ea928a108d2
sagar9980274589 <sagarshaivaooo@gmail.com>
2026-04-21 21:15:40 +0530

    Address

---
+++ Country task/src/com/xworkz/country/Address.java
@@ -0,0 +1,13 @@
+package com.xworkz.country;
+
+public class Address {
+    int no;
+    long pinCode;
+    State state;
+    public Address(int no, long pinCode, State state) {
+        this.no = no;
+        this.pinCode = pinCode;
+        this.state = state;
+
+    }
+}
