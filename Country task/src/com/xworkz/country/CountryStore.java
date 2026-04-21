6f9b4cf26f3563e7f3090ca373803a3dee961f98
sagar9980274589 <sagarshaivaooo@gmail.com>
2026-04-21 21:16:23 +0530

    Country Store

---
+++ Country task/src/com/xworkz/country/CountryStore.java
@@ -0,0 +1,61 @@
+package com.xworkz.country;
+/*
+ save(Country country);\n
+ Country findByName(String name)\n
+  State findStateByStateName(String name)\n
+   City[] findAllCityByStateName(String name) int findNoOfDistrictsByCityName(String name)\n
+   Country findByCollectorName(String collectorName)\n
+Submission Instructions:
+ */
+public class CountryStore {
+    Country[] countries=new Country[10];
+    int index=0;
+
+    void save(Country country){
+        if(country!=null){
+           if(this.countries!=null){
+               if(index<this.countries.length){
+                   this.countries[this.index]=country;
+                   this.index++;
+               }
+               else{
+                   System.out.println("Array is full");
+               }
+           }
+           else{
+               System.out.println("Country object is null");
+           }
+        }
+        else{
+            System.out.println("Country is null");
+        }
+    }
+
+   Country getByName(String name) {
+       if(this.countries!=null) {
+           if(name!=null){
+               for(Country country:this.countries){
+                   if(country!=null){
+                       if(name.equals(country.name)){
+                           return country;
+                       }
+                   }
+
+               }
+
+           }else{
+               System.out.println("Country name is null");
+           }
+
+       }
+       else{
+           System.out.println("array is empty");
+       }
+       System.out.println("Country not present");
+     return null;
+   }
+
+
+
+
+}
