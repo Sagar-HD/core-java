2e1a6a5d5350c1157fb7b9ebb4b72fd14d3ed72b
sagar9980274589 <sagarshaivaooo@gmail.com>
2026-05-04 20:07:34 +0530

    hospital

---
+++ Interfaces/src/com/xworkz/interfaces/Hospital.java
@@ -0,0 +1,11 @@
+package com.xworkz.interfaces;
+
+public interface Hospital {
+int MAX_PATIENTS = 100;
+int DOCTORS = 20;
+
+void admitPatient();
+void dischargePatient();
+void provideTreatment();
+}
+
